1953e5e01891881890178f14231dcb4516046660
Suvasish Manna <suvasishmanna@users.noreply.github.com>
2019-11-09 13:40:24 -0500

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -41,7 +41,6 @@ export const IndexPageTemplate = ({
           style={{
             boxShadow:
               'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
